3ec44056b3faade773241732aa5a26dabd3b26c3
cullyfung <cullyfung@gamil.com>
2023-09-29 16:46:59 +0800

    feat: layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,7 +5,9 @@ import { useMemo } from 'react';
 
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+import SidebarItem from './SidebarItem';
 import Box from './Box';
+import Library from './Library';
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -46,8 +48,26 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           p-2
         "
       >
-        <Box>hello</Box>
+        <Box>
+          <div
+            className="
+              flex
+              flex-col
+              gap-y-4
+              px-5
+              py-4         
+            "
+          >
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
       </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
